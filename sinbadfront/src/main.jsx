1ef843d7b3cea83c4258986b199d32afc9db6de5
ccmar <ccmdehoop1991@gmail.com>
2023-07-17 14:14:49 +0200

    Basic package

--- sinbadfront/src/main.jsx
+++ sinbadfront/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+import './scss/mainSheet.scss'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      {/* <Route path="/:id" element={<Show />} /> */}
+    </Routes>
+  </BrowserRouter>
+);
