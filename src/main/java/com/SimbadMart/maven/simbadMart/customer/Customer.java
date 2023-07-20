29f61ea8de5738619875233861e8164867a8ab71
Alexandernevsk <Alexandertorreman@gmail.com>
2023-07-20 12:57:45 +0200

    Check for gitignore

--- src/main/java/com/SimbadMart/maven/simbadMart/customer/Customer.java
+++ src/main/java/com/SimbadMart/maven/simbadMart/customer/Customer.java
@@ -1,8 +1,6 @@
 package com.SimbadMart.maven.simbadMart.customer;
 
 import com.SimbadMart.maven.simbadMart.ShopCart.ShopCart;
-import com.SimbadMart.maven.simbadMart.storage.Storage;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -21,10 +19,39 @@ public class Customer {
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
     private List<ShopCart> shopCart = new ArrayList<>();
+
     public Customer() {
     }
 
-//    public List<ShopCart> getShopCart() {
-//        return shopCart;
-//    }
+    public Customer(String customerName, String customerSurname, String e_mail, String address, int phone, List<ShopCart> shopCart) {
+        this.customerName = customerName;
+        this.customerSurname = customerSurname;
+        this.e_mail = e_mail;
+        this.address = address;
+        this.phone = phone;
+        this.shopCart = shopCart;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerSurname() {
+        return customerSurname;
+    }
+
+    public String getE_mail() {
+        return e_mail;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+    public List<ShopCart> getShopCart() {
+        return shopCart;
+    }
 }
