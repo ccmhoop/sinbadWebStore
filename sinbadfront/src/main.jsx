a2e98201676bc81816d8982b7820b8b173cb69e7
Alexandernevsk <Alexandertorreman@gmail.com>
2023-07-24 10:46:28 +0200

    Check for gitignore

--- sinbadfront/src/main.jsx
+++ sinbadfront/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./scss/mainSheet.scss";
-import ShopCart from "./pages/Shopcart";
+import Register from "./pages/Register";
+import Account from "./pages/Account"
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,7 +13,9 @@ root.render(
     <Routes>
       <Route index element={<Home />} />
       {/* <Route path="/:id" element={<Show />} /> */}
-      <Route path="/Shopcart" element={<ShopCart />} />
+      {/* <Route path="/Shopcart" element={<ShopCart />} /> */}
+      <Route path="/register" element={<Register />} />
+      <Route path="/account" element={<Account/>}/>
     </Routes>
   </BrowserRouter>
 );
