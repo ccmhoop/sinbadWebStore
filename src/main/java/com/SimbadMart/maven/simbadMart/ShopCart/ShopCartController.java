bd974b0df04d0b77a7fec1beeca3312bedc25701
ccmar <ccmdehoop1991@gmail.com>
2023-07-26 13:26:02 +0200

    updated pre merge database

--- src/main/java/com/SimbadMart/maven/simbadMart/ShopCart/ShopCartController.java
+++ src/main/java/com/SimbadMart/maven/simbadMart/ShopCart/ShopCartController.java
@@ -1,16 +1,28 @@
 package com.SimbadMart.maven.simbadMart.ShopCart;
 
+import com.SimbadMart.maven.simbadMart.customer.CustomerRepository;
+import com.SimbadMart.maven.simbadMart.storage.StorageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/shop")
 public class ShopCartController {
 
     @Autowired
     ShopCartRepository shopCartRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    StorageRepository storageRepository;
+    @PostMapping("save/{customerId}/{storageID}/{amount}")
+    public ShopCart saveShopCart(@PathVariable("customerId") Long customerId, @PathVariable("storageID") Long storageID, @PathVariable("amount")  int amount) {
+        return shopCartRepository.save(new ShopCart(amount,storageRepository.findById(storageID).get() ,customerRepository.findById(customerId).get()));
+    }
+
+//    @PostMapping("save")
+//    public ShopCart saveShopCart(@RequestBody ShopCart shopCart)
+//    { return shopCartRepository.save(shopCart); }
 
 }
