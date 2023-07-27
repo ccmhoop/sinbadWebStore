f3fa48fd5260c86b640dde2b8b9de3d104c059f8
Alexandernevsk <Alexandertorreman@gmail.com>
2023-07-27 13:17:06 +0200

    Check for gitignore

--- sinbadfront/src/components/UserInfo.jsx
+++ sinbadfront/src/components/UserInfo.jsx
@@ -9,14 +9,43 @@ export default function UserInfo({
   customerSurname,
   customerName,
   id,
+  ChangeInfo,
 }) {
+
+const handleChange = (e) => {
+    e.target.value;
+}
+
+
   return (
-    <div key={id} className="home-box-style">
-      <p className="details"> <span>E-mail:</span> {eMail}</p>
-      <p className="details"> <span>Adress:</span> {address}</p>
-      <p className="details"> <span>Phone:</span> {phone}</p>
-      <p className="details"> <span>Birth date:</span>  {dateOfBirth}</p>
-      <p className="details"> <span>Name:</span> {customerName} {customerSurname} </p>
+    <div key={id} className="home-box-style"> 
+      <p className="details">
+         <span>Name:</span> {customerName} {customerSurname} 
+         </p>
+      <p className="details"> 
+        <span>Birth date:</span>  {dateOfBirth} 
+        </p>
+      <p className="details"> 
+        <span>E-mail:</span> {eMail} 
+        <input type="text" 
+            value={eMail}
+            onChange={handleChange}
+        />
+        <span className="info-button"></span>
+        <button>change email </button>
+        </p>
+      <p className="details"> 
+        <span>Adress:</span> {address}
+        <input type="text" /> 
+        <span className="info-button"></span>
+        <button>change address</button>
+        </p>
+      <p className="details"> 
+        <span>Phone:</span> {phone} 
+        <input type="text" />
+        <span className="info-button"></span>
+        <button>change phone</button>
+        </p>
     </div>
   );
 }
